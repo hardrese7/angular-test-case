7b1fe9c49bd17908b42a336bb9c536643c47c82c
Rodion Golovushkin <hard.rese7@gmail.com>
2017-03-19 17:17:23 +0700

    changes in markup and logic in clients list

--- src/app/clients-list/clients-list.component.ts
+++ src/app/clients-list/clients-list.component.ts
@@ -1,3 +1,6 @@
+import { ClientsService } from '../clients.service';
+import { AppService } from '../app.service';
+import { Client } from "../models"
 import {
   Component,
   OnInit,
@@ -9,8 +12,26 @@ import {
   styleUrls: ['./clients-list.component.scss']
 })
 export class ClientsListComponent implements OnInit {
+  constructor(private appService: AppService, private clientsService: ClientsService) {
 
+  }
+  clients: Client[];
+  errorMessage: any;
+  selectedClient: Client;
   public ngOnInit() {
+    this.getClients();
+  }
+  getClients() {
+    this.appService.getClients()
+      .subscribe(
+      (clients) => {
+        this.clients = clients
+      },
+      (error) => this.errorMessage = <any>error);
+  }
+  selectClient(client: Client){
+    this.selectedClient = client;
+    this.clientsService.selectClient(client);
   }
 
 }
